b2eee57277b8b7f794e5848466389cc3da83df4c
LouiseKlodt <louisekdublin@gmail.com>
2020-08-13 12:09:59 +0100

    update readme

---
+++ resources/public/js/bgsound.js
@@ -0,0 +1,50 @@
+function themesound() {
+  //console.log('bgsound!');
+  var sound = new Howl({
+    src: ['/static/space-invaders.mp3'],
+    autoplay: false,
+    loop: true,
+    volume: 0.6,
+    onend: function() {
+      //console.log('Finished!');
+    }
+  });
+  return sound;
+}
+// var soundid = sound.play();
+
+function bangsound() {
+  //console.log('bangsound!');
+  var explosion = new Howl({
+    src: ['/static/explosion.wav'],
+    volume: 0.5,
+    onend: function() {
+      //console.log('Explosion!');
+    }
+  });
+  return explosion;
+}
+
+function shootsound() {
+  //console.log('shootsound!');
+  var shoot = new Howl({
+    src: ['/static/shoot.wav'],
+    volume: 0.5,
+    onend: function() {
+      //console.log('Shooting!');
+    }
+  });
+  return shoot;
+}
+
+function hissingsound() {
+  //console.log('hissingsound!');
+  var hiss = new Howl({
+    src: ['/static/tankhit.wav'],
+    volume: 0.5,
+    onend: function() {
+      //console.log('Collision!');
+    }
+  });
+  return hiss;
+}
